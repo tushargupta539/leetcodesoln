9cc80329c016fc6f68cbe71dacb5066b9713a146
tushargupta539 <tusharleetcodeprofile@gmail.com>
2026-06-13 17:41:58 +0530

    Time: 119 ms (5.5%), Space: 104.5 MB (7.22%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        for(auto it:nums)
+        {
+            if(mp[it]>1)return it;
+        }
+        return 1;
+    }
+};
